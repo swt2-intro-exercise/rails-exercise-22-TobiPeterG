99554fedf187d4994f601cbcfe483e773e1e971e
TobiPeterG <tobi.goergens@gmail.com>
2022-11-14 16:00:28 +0100

    Fix isue #4

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,12 +1,18 @@
 class AuthorsController < ApplicationController
   def new
   end
+  def index
+    @authors = Author.all
+  end
   def create
   @author = Author.new(author_params)
 
   @author.save
   redirect_to @author
 end
+ def show
+    @author = Author.find(params[:id])
+  end
 private
   def author_params
     params.require(:author).permit(:first_name, :last_name, :homepage)
